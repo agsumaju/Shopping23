aec97394f8f27a3101cc3ee8fb9a1c074c76f5b6
juan manuel suarez aguilar <agsumaju@gmail.com>
2023-10-24 14:10:20 -0500

    Create DataBase and CRUD Countries

---
+++ Shopping23/Shopping23/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping23.Data.Entities;
+
+namespace Shopping23.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
